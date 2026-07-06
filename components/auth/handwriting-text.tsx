3e6d40edd3af1ac9786701643ee64413fd697ebb
Sasitha Ruvin <sasitharuween@gmail.com>
2026-07-06 14:45:08 +0530

    1.0.0

--- components/auth/handwriting-text.tsx
+++ components/auth/handwriting-text.tsx
@@ -6,25 +6,10 @@ export function HandwritingText() {
   return (
     <div className="relative">
       <div className="font-sans text-[72px] leading-[1.1] font-semibold tracking-wide text-[var(--ds-label)]">
-        <div className="whitespace-nowrap">
-          {title.split("").map((char, i) => (
-            <span
-              key={`t-${i}`}
-              className="inline-block opacity-0 translate-y-2.5 animate-[writeIn_0.3s_ease-out_forwards]"
-              style={{ animationDelay: `${i * 0.15}s` }}
-            >
-              {char === " " ? "\u00A0" : char}
-            </span>
-          ))}
-        </div>
+        {title}
       </div>
 
-      <p
-        className="mt-7 font-sans text-[17px] tracking-wide text-[#6C6C70] opacity-0 animate-[fadeIn_0.8s_ease-out_forwards]"
-        style={{
-          animationDelay: `${title.length * 0.15 + 0.3}s`,
-        }}
-      >
+      <p className="mt-7 font-sans text-[17px] tracking-wide text-[#6C6C70]">
         Project Management Platform
       </p>
     </div>
